b5bf4b8c6ffb70a11e28838c97f5cd3ca508b045
fhnaseer <fhnaseer@live.com>
2019-04-12 11:25:53 +0200

    Adding problematic nuget package,

--- Console/Console.Framework/Program.cs
+++ Console/Console.Framework/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace Console.Framework
+﻿using System;
+using System.Diagnostics;
+
+namespace Console.Framework
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            var size = IntPtr.Size;
+            using (var image = new ImageMagick.MagickImage(@"C:\APEXImage\Data\alpha\bottom.png"))
+            {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                image.Blur(10, 3);
+                stopwatch.Stop();
+                System.Console.WriteLine($"Blur Time: {stopwatch.ElapsedMilliseconds}");
+                image.Write(@"C:\Users\FHZ2\Desktop\blur.png");
+            }
         }
     }
 }
