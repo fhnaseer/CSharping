29023dd2408cb13a03d7f5640fdbdb388c0d1938
fhnaseer <fhnaseer@live.com>
2021-03-04 11:12:47 +0100

    ExcludeFromCodeCoverage tests,

---
+++ Tests/UnitTests.Core/IncludedClassTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Core
+{
+    [TestClass]
+    public class IncludedClassTest
+    {
+        [TestMethod]
+        public void SomeTest()
+        {
+            // Arrange,
+            var target = new IncludedClass();
+
+            // Act,
+            target.IncludedProperty = target.ExcludedProperty;
+            target.ExcludedProperty = target.IncludedProperty;
+
+            target.IncludedMethod(target.IncludedProperty);
+            target.ExcludedMethod(target.IncludedProperty);
+        }
+    }
+}
