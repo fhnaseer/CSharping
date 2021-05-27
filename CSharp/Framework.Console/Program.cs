29b23c5f7857d544a78fde6634ea2d969d74409b
fhnaseer <fhnaseer@live.com>
2021-05-27 15:36:28 +0200

    Adding wpf applications,

--- CSharp/Framework.Console/Program.cs
+++ CSharp/Framework.Console/Program.cs
@@ -1,23 +1,21 @@
-﻿using Microsoft.VisualStudio.Coverage.Analysis;
-using System.Diagnostics;
-
-namespace Console.Framework
+﻿namespace Console.Framework
 {
     internal static class Program
     {
         private static void Main()
         {
-            var path = @"D:\Code\MSTest\src\Vanguard\test\Vanguard.Net.Tests\TestFiles\CoverageFiles\Sample2.coverage";
-            var output = @"D:\Code\ResultsInterop2.xml";
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            using (CoverageInfo info = CoverageInfo.CreateFromFile(path))
-            {
-                CoverageDS data = info.BuildDataSet();
-                data.WriteXml(output);
-            }
-            stopwatch.Stop();
-            System.Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds}");
+            System.Console.WriteLine(System.Environment.OSVersion.Version);
+            //var path = @"D:\Code\MSTest\src\Vanguard\test\Vanguard.Net.Tests\TestFiles\CoverageFiles\Sample2.coverage";
+            //var output = @"D:\Code\ResultsInterop2.xml";
+            //var stopwatch = new Stopwatch();
+            //stopwatch.Start();
+            //using (CoverageInfo info = CoverageInfo.CreateFromFile(path))
+            //{
+            //    CoverageDS data = info.BuildDataSet();
+            //    data.WriteXml(output);
+            //}
+            //stopwatch.Stop();
+            //System.Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds}");
         }
     }
 }
