6998799934be0ad1dccf3c12f91adba1e08315d2
fhnaseer <fhnaseer@live.com>
2022-09-05 17:00:33 +0200

    updating nuget packages,

--- CSharp/Framework.UnitTests/WebApiTest.cs
+++ CSharp/Framework.UnitTests/WebApiTest.cs
@@ -1,16 +1,15 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Net;
 
 namespace Framework.UnitTests
 {
     [TestClass]
     public class WebApiTest
     {
-        [TestMethod]
-        public void WebApi_Test()
-        {
-            var request = (HttpWebRequest)HttpWebRequest.Create("http://localhost/Framework.WebApi");
-            request.GetResponse();
-        }
+        //[TestMethod]
+        //public void WebApi_Test()
+        //{
+        //    var request = (HttpWebRequest)HttpWebRequest.Create("http://localhost/Framework.WebApi");
+        //    request.GetResponse();
+        //}
     }
 }
