15662dcaeed1987d09ed515a018cd619d17601ec
fhnaseer <fhnaseer@live.com>
2024-04-29 15:50:08 +0200

    Using mstest runner,

---
+++ CSharp/ClassLibrary.Fakes.Tests/WebApiTest.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary.Tests;
+
+[TestClass]
+public class WebApiTest
+{
+    //[TestMethod]
+    //public void WebApi_Test()
+    //{
+    //    var request = (HttpWebRequest)HttpWebRequest.Create("http://localhost/Framework.WebApi");
+    //    request.GetResponse();
+    //}
+}
